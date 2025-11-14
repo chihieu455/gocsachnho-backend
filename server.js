a45a4edf37fdb651f7d2243861d365d14c5a1981
chihieu455 <23050005@student.bdu.edu.vn>
2025-11-14 09:15:40 +0700

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const apiRoutes = require('./routes/api');
+const apiRoutes = require('api');
 
 const app = express();
 
